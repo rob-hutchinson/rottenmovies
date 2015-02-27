dbdf316567e1e383e18eb7376f28f9abd5d84a86
matthew gould <gouldm44@gmail.com>
2015-02-27 14:17:52 -0500

    Added endpoints and some erb stuff

---
+++ rottenmovies.rb
@@ -0,0 +1,30 @@
+require 'sinatra/base'
+require 'pry'
+
+require './db/setup'
+require './lib/all'
+
+
+class Rottenmovies < Sinatra::Base
+  
+  get '/' do
+
+  erb :upcoming
+  end
+
+  get '/users' do
+
+  erb :profile
+  end
+
+  get 'movies' do
+
+  erb :movie
+  end
+
+  not_found do
+    status 404
+    erb :nope
+  end
+
+end
