eb41e356799afa74103f8255c4b199ea990a6c3c
Wonman Lee <wonmanlee@gmail.com>
2015-02-28 15:39:48 -0500

    Movie Page created, still needs to be integrated further

--- rottenmovies.rb
+++ rottenmovies.rb
@@ -32,6 +32,16 @@ class Rottenmovies < Sinatra::Base
     erb :movie
   end
 
+  get '/movies/:rotten_id' do
+    @movie = Movie.find_by(rotten_id: params[:rotten_id])
+    if @movie
+      @comments = Comment.where(movie_id: @movie.id)
+      erb :movie_page
+    else
+      erb :nope
+    end
+  end
+
   post '/movies' do
     new_comment = Comment.create_comment!(params["comment_string"], current_user, current_movie)
     erb :movie
