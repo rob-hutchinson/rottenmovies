98b4ff2ae14dbaed77abe6eb44981d6d77e288ba
Mishka <mishka.s.mukherji@gmail.com>
2015-02-28 14:42:06 -0500

    added changes to comment_tests

---
+++ test/comment_tests.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'rack/test'
+
+# require 'active_record'
+require './rottenmovies'
+
+require 'pry'
+
+class CommentTest < MiniTest::Test
+
+ include Rack::Test::Methods
+  # def setup
+  #   super
+  #   # User.create! email: 'brit@kingcons.io', password: 'hunter2', name: 'Brit Butler'
+  # end
+
+  def app
+    Rottenmovies
+  end
+
+  def test_user_has_comments
+    user = User.first
+    Comment.create_comment!(comment: "this is my comment", user_id: user.id, movie_id: 200)
+    assert_equal user.comments.last.comment, "this is my comment"
+  end
+  
+  def test_movies_have_comments
+    user = User.first
+    movie = Movie.create!(title: "Asshole")
+    Comment.create_comment!(comment: "this is my comment", user_id: user.id, movie_id: movie.id)
+    assert_equal movie.comments.last.comment, "this is my comment"
+  end
+end
