aca0d6da1506b9d1b8a0d0e9eefcf433c645f847
matthew gould <gouldm44@gmail.com>
2015-03-01 18:48:38 -0500

    Added profile stuff that looks pretty decent.

--- lib/user.rb
+++ lib/user.rb
@@ -7,7 +7,11 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   validates :email, presence: true
   validates :username, uniqueness: true
+  validates :location, length: { maximum: 20 }
+
+  def defaults
+    self.location ||= "The Iron Yard, D.C."
+    self.bio ||= "Member of The IronYard D.C. branch."
+  end
 
 end
-
-
